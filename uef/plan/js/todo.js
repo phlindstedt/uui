294ecb1e85a8d8f8b825b1054a62c3bcc50e56c5
phlindstedt <plindstedt@troop.se>
2018-10-17 16:00:43 +0200

    .

--- uef/plan/js/todo.js
+++ uef/plan/js/todo.js
@@ -62,7 +62,7 @@ data = data || {};
                 removeElement(object);
 				console.log('elem: ' + todo.element);	
                 delete data[id];
-				console.log('data' + data[id]);
+				console.log('data' + data.length);
                 $("#" + defaults.deleteDiv).hide();
             }
         })
@@ -135,7 +135,7 @@ data = data || {};
 		if (todo.elements[i].id === params.id) {
 			console.log('deleting: ' + todo.elements[i].id);
 			todo.elements.splice(i, 1);
-			console.log('todo: ' + todo.elements[i].id);
+			console.log('todo: ' + todo.elements.length);
 			break;
 		}
 		
